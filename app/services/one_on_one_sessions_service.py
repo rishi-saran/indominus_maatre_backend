e916cd28cc2cd35aade98c4537973dda6a10c541
Sanjay Kumar SS <sanjay.savitha05@gmail.com>
2026-02-24 09:30:07 +0530

    feat: assign priest service

--- app/services/one_on_one_sessions_service.py
+++ app/services/one_on_one_sessions_service.py
@@ -1,13 +1,26 @@
 # /app/services/one_on_one_sessions_service.py
 
-import datetime 
+from datetime import datetime 
 from uuid import UUID
 from app.core.supabase import supabase
 
 def assign_priest(
-    start_time: datetime.datetime, 
-    end_time :datetime.datetime
+    start_time: datetime,
+    end_time: datetime
 ) -> UUID:
-    # Return the UUID of the assigned priest
 
-    return None
+    day_of_week = start_time.weekday() #* 0 -> monday, 6 -> sunday
+
+    query = supabase.rpc(
+        "assign_next_priest",
+        {
+            "p_day_of_week": day_of_week,
+            "p_start_time": start_time.isoformat(),
+            "p_end_time": end_time.isoformat(),
+        }
+    ).execute()
+
+    if not query.data:
+        raise ValueError("No priest available")
+
+    return UUID(query.data[0]["priest_id"])
